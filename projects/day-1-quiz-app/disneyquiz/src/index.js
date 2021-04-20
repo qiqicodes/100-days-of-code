d64c6fba9512ef22647e8c3a2cefcabdad17e60b
qiqicodes <qiqizheng0127@gmail.com>
2021-04-19 21:12:10 -0700

    day 1 quiz app init

---
+++ projects/day-1-quiz-app/disneyquiz/src/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+
+class DisneyQuiz extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="title">Disney Quiz</div>
+      </div>
+    )
+  }
+}
+
+
+
+
